08ab084497f01f0c9e54101a6160c72cf0c9c5ab
soriphoono <soriphoono@protonmail.com>
2025-04-28 11:57:24 -0500

    Updated to flask

--- main.py
+++ main.py
@@ -3,15 +3,11 @@ from flask import Flask, request, url_for, render_template, redirect
 app = Flask(__name__)
 
 
-@app.route("/", methods=["GET"])
+@app.route("/", methods=["GET", "POST"])
 def hello_world():
-    return render_template("index.html")
-
-@app.route('/api/contact', methods=['POST'])
-def contact():
     if request.method == 'POST':
         name = request.form['name']
         email = request.form['email']
         comments = request.form['comments']
-
-        print(name, email, comments)
+    elif request.method == 'GET':
+        return render_template("index.html")
