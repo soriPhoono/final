faacb4e183dc6dc46f02f5a6c9849c222328896e
soriphoono <soriphoono@protonmail.com>
2025-04-28 12:16:09 -0500

    Updated nav bar

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from flask import Flask, request, url_for, render_template, redirect
+from sqlite3 import connect
 
 app = Flask(__name__)
 
+connection = connect('database.db')
 
-@app.route("/", methods=["GET", "POST"])
+
+@app.route("/")
 def hello_world():
-    if request.method == 'POST':
-        print("test")
+    return render_template("index.html")
 
-        return render_template("index.html")
-    elif request.method == 'GET':
-        return render_template("index.html")
+@app.route("/about")
+def about():
+    return render_template("about.html")
